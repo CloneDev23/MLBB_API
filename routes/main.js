43cca778390095f02f4e1b1663ba166e8a6c440f
CloneDev23 <92073273+CloneDev23@users.noreply.github.com>
2022-01-08 23:57:59 +0800

    Update and rename plugin/main.js to routes/main.js

---
+++ routes/main.js
@@ -0,0 +1,33 @@
+__path = process.cwd()
+const express = require('express');
+const router = express.Router();
+
+// Scrapernya
+const { herodetails } = require(__path + '/plugin/herodetail')
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+    	status: false,
+    	message: "Sorry, the page you are looking for doesn't exist",
+      feature: {
+        	herodetail: "https://" + req.hostname + "/herodetail?query=Zilong"
+      }
+})
+})
+
+router.get('/herodetail', async (req, res, next) => {
+     const query = req.query.query;
+           
+     herodetails(query).then(result => {
+        res.status(200).send({creator: "Kotzyy", status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+})
+
+
+module.exports = router
