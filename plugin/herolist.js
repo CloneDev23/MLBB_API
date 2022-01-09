405b3059793096932e7db855999b5bff11884c8a
CloneDev23 <92073273+CloneDev23@users.noreply.github.com>
2022-01-09 13:13:08 +0800

    Update herolist.js

--- plugin/herolist.js
+++ plugin/herolist.js
@@ -7,14 +7,16 @@ function herolist(){
                   .then(({ data }) => {
                        const $ = cheerio.load(data)
                        let data_hero = []
+                       let role_hero = []
                        let url = []
                        $('div > div > span > span > a').get().map((result) => {
                             const name = decodeURIComponent($(result).attr('href').replace('/wiki/',''))
                             const urln = 'https://mobile-legends.fandom.com' + $(result).attr('href')
                             data_hero.push(name)
+                            role_hero.push(role)
                             url.push(urln)
                        })
-                       resolve({ status: 200, hero: data_hero })
+                       resolve({ status: 200, hero: data_hero , role: role_hero })
                   }).catch((e) => reject({ status: 404, message: e.message }))
              })
         }
